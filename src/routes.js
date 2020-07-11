b96c6f61a03cb2fa12e5c32d3effd4ddd4933a99
Hữu Tôn <nghuuton@gmail.com>
2020-07-11 23:15:14 +0700

    Stripes animation

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Layout from "./Hoc/Layout";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
 import Home from "./Components/home";
 
-function Routes() {
+const Routes = (props) => {
   return (
     <Layout>
       <Switch>
-        <Redirect exact from="/" to="/home" />
-        <Route path="/home" component={Home} />
+        <Route exact component={Home} path="/" />
       </Switch>
     </Layout>
   );
-}
+};
 
 export default Routes;
