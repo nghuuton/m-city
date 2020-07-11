8aa62a507236316abd30e849ce6e4714b544d965
Hữu Tôn <nghuuton@gmail.com>
2020-07-12 00:36:39 +0700

    tag component && section team init

---
+++ src/Components/home/matches/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Tag } from "../../ui/misc";
+
+const MatchesHome = () => {
+  return (
+    <div className="home_matches_wrapper">
+      <div className="container">
+        <Tag bck="#0c1731" size="50px" color="#ffffff">
+          Matches
+        </Tag>
+        BLOCK
+        <Tag
+          bck="#ffffff"
+          size="22px"
+          color="#0c1731"
+          link={true}
+          linkTo="/the-team"
+        >
+          See more matches
+        </Tag>
+      </div>
+    </div>
+  );
+};
+
+export default MatchesHome;
