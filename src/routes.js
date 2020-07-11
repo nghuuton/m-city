8315c0473b639ce96d4d508c7eee26e1084ed85a
Hữu Tôn <nghuuton@gmail.com>
2020-07-11 21:36:12 +0700

    Footer finish

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Layout from "./Hoc/Layout";
+import { Switch, Route } from "react-router-dom";
 
 function Routes() {
-  return <Layout>Hello</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route />
+      </Switch>
+    </Layout>
+  );
 }
 
 export default Routes;
