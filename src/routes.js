9810e483dcc8010e257c5abcbe49976703698564
Hữu Tôn <nghuuton@gmail.com>
2020-07-11 21:53:45 +0700

    init stripes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "./Hoc/Layout";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./Components/home";
 
 function Routes() {
   return (
     <Layout>
       <Switch>
-        <Route />
+        <Redirect exact from="/" to="/home" />
+        <Route path="/home" component={Home} />
       </Switch>
     </Layout>
   );
