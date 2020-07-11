bea03bc4dbfd3dcc0d33804afcf2562637e4e5df
Hữu Tôn <nghuuton@gmail.com>
2020-07-11 20:44:26 +0700

    Finish Header

--- src/Components/Header_footer/Header.jsx
+++ src/Components/Header_footer/Header.jsx
@@ -4,6 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
 import { Link } from "react-router-dom";
+import { CityLogo } from "../ui/icons";
 
 export class Header extends Component {
   render() {
@@ -19,7 +20,9 @@ export class Header extends Component {
       >
         <Toolbar style={{ display: "flex" }}>
           <div style={{ flexGrow: 1 }}>
-            <div className="header_logo">LOGO</div>
+            <div className="header_logo">
+              <CityLogo link={true} linkTo="/" width="70px" height="70px" />
+            </div>
           </div>
           <Link to="/the-team">
             <Button color="inherit">The Team</Button>
