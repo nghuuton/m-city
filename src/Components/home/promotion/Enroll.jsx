e87776dbb9063e6d733929e3e1f1d2641dff31b5
Hữu Tôn <nghuuton@gmail.com>
2020-07-12 15:36:59 +0700

    Enroll firebase submit

--- src/Components/home/promotion/Enroll.jsx
+++ src/Components/home/promotion/Enroll.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Fade from "react-reveal/Fade";
 import FormField from "../../ui/formField";
 import { validate } from "../../ui/misc";
+import { firebasePromotions } from "../../../firebase";
 
 class Enroll extends Component {
     state = {
         formError: false,
-        formSuccess: "",
+        formSuccess: false,
         formdata: {
             email: {
                 element: "input",
@@ -26,6 +27,17 @@ class Enroll extends Component {
             },
         },
     };
+
+    // componentDidMount() {
+    //     firebasePromotions
+    //         .orderByChild("email")
+    //         .equalTo("nghuuton@gmail.com")
+    //         .once("value")
+    //         .then((snapshot) => {
+    //             console.log(snapshot.val());
+    //         });
+    // }
+
     updateForm(element) {
         // console.log(element);
         const newFormdata = { ...this.state.formdata };
@@ -45,16 +57,53 @@ class Enroll extends Component {
             formdata: newFormdata,
         });
     }
+
+    ressetFormSuccess(type) {
+        const newFormdata = { ...this.state.formdata };
+        for (let key in this.state.formdata) {
+            newFormdata[key].value = "";
+            newFormdata[key].valid = false;
+            newFormdata[key].validationMessage = "";
+        }
+        this.setState({
+            formError: false,
+            formdata: newFormdata,
+            formSuccess: type ? "Congratulations" : "Already on the database",
+        });
+        this.successMessage();
+    }
+
+    successMessage() {
+        setTimeout(() => {
+            this.setState({
+                formSuccess: "",
+            });
+        }, 2000);
+    }
+
     submitForm(event) {
         event.preventDefault();
-        let dataSubmit = {};
+        let dataToSubmit = {};
         let formIsvalid = true;
         for (let key in this.state.formdata) {
-            dataSubmit[key] = this.state.formdata[key].value;
+            dataToSubmit[key] = this.state.formdata[key].value;
             formIsvalid = this.state.formdata[key].valid && formIsvalid;
         }
         if (formIsvalid) {
-            console.log(dataSubmit);
+            firebasePromotions
+                .orderByChild("email")
+                .equalTo(dataToSubmit.email)
+                .once("value")
+                .then((snapshot) => {
+                    // console.log(snapshot.val());
+                    if (snapshot.val() === null) {
+                        firebasePromotions.push(dataToSubmit);
+                        this.ressetFormSuccess(true);
+                    } else {
+                        this.ressetFormSuccess(false);
+                    }
+                });
+            // this.ressetFormSuccess();
         } else {
             this.setState({
                 formError: true,
@@ -79,9 +128,17 @@ class Enroll extends Component {
                                 Something is wrong, try again.
                             </div>
                         ) : null}
+                        <div className="success_label">
+                            {this.state.formSuccess}
+                        </div>
                         <button onClick={(event) => this.submitForm(event)}>
                             Enroll
                         </button>
+                        <div className="enroll_discl">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Ratione explicabo ad sequi a consequuntur
+                            eaque?
+                        </div>
                     </form>
                 </div>
             </Fade>
